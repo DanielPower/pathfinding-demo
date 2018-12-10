5555efe464b6c88024e037674859c60626e8b21c
Daniel Power <me@danielpower.ca>
2018-12-10 17:01:13 -0330

    Try to hook pathfinders into gui... Currently BORK

--- src/gui.cpp
+++ src/gui.cpp
@@ -2,20 +2,33 @@
 
 Gui::Gui()
 {
+	// Create window
 	window.create(sf::VideoMode(1280, 720), "COMP 3200");
+
+	// Create map from file
 	Map map = Image::load("map.bmp");
 
-	astar = std::make_shared<AStar>(map);
-	bfs = std::make_shared<BFS>(map);
-	pathfinders = {bfs, astar};
+	// Setup vector of pathfinders
+	pathfinders = {
+		std::make_shared<BFS>(map),
+		std::make_shared<AStar>(map)
+	};
 
-	view.reset(sf::FloatRect(0.f, 0.f, map.width, map.height));
-	view.setViewport(sf::FloatRect(0.f, 0.f, map.width/1280.f, map.height/720.f));
-	zoom = 1;
+	// Initialize currentPathfinder as instance of BFS
+	currentPathfinder = pathfinders[gui_BFS];
 
-	map_image.create(map.width, map.height, sf::Color::Black);
+	// DEBUG - set start/end until implemented in gui
+	auto start = map.get(5, 5);
+	auto end = map.get(25, 25);
+	currentPathfinder->setGoal(start, end);
+	currentPathfinder->step();
+
+	// Create blank images
+	map_image.create(map.width, map.height, sf::Color(0, 0, 0, 0));
 	open_image.create(map.width, map.height, sf::Color(0, 0, 0, 0));
 	closed_image.create(map.width, map.height, sf::Color(0, 0, 0, 0));
+
+	// Fill map_image with data from map
 	for (uint x=0; x<map.width; x++)
 	{
 		for (uint y=0; y<map.height; y++)
@@ -35,14 +48,23 @@ Gui::Gui()
 			}
 		}
 	}
+
+	// Create map sprite
 	map_texture.loadFromImage(map_image);
 	map_sprite.setTexture(map_texture);
 
+	// Create blank open sprite
 	open_texture.loadFromImage(open_image);
 	open_sprite.setTexture(open_texture);
 
+	// Create blank closed sprite
 	closed_texture.loadFromImage(closed_image);
 	closed_sprite.setTexture(closed_texture);
+
+	// Set initial view
+	view.reset(sf::FloatRect(0.f, 0.f, map.width, map.height));
+	view.setViewport(sf::FloatRect(0.f, 0.f, map.width/1280.f, map.height/720.f));
+	zoom = 1;
 }
 
 bool Gui::update()
@@ -81,7 +103,8 @@ bool Gui::update()
 			{
 				switch(event.key.code)
 				{
-					case(sf::Keyboard::Space): { break; }
+					case(sf::Keyboard::Space): { pathfindingStep(); break; }
+					case(sf::Keyboard::Escape): { window.close(); break; }
 					default: { break; }
 				}
 			}
@@ -109,15 +132,24 @@ void Gui::render()
 
 void Gui::pathfindingStep()
 {
+	// FIXME - Crashes here, even though currentPathfinder->step() works fine if run in Gui::Gui()
+	currentPathfinder->step();
+
+	// Clear open and closed images
 	open_image.create(map.width, map.height, sf::Color(0, 0, 0, 0));
 	closed_image.create(map.width, map.height, sf::Color(0, 0, 0, 0));
 
-	for (auto tile : pathfinders[gui_BFS]->getOpenList())
+	std::cout << currentPathfinder->getOpenList().size() << std::endl;
+
+	// Add yellow pixel to open_image for each open tile
+	for (auto tile : currentPathfinder->getOpenList())
 	{
+		std::cout << "tile " << tile->getX(map.width) << " " << tile->getY(map.width) << std::endl;
 		open_image.setPixel(tile->getX(map.width), tile->getY(map.width), sf::Color(255, 0, 0));
 	}
 
-	for (auto tile : pathfinders[gui_BFS]->getClosedList())
+	// Add green pixel to closed_image for each closed tile
+	for (auto tile : currentPathfinder->getClosedList())
 	{
 		closed_image.setPixel(tile->getX(map.width), tile->getY(map.width), sf::Color(0, 255, 255));
 	}
