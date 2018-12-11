c119c5e60cbc46bb5fec2ac920d175dcb845657a
Daniel Power <me@danielpower.ca>
2018-12-11 05:03:43 -0330

    Setup relative imports, and prepare for use of imgui

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+rm bin/main
 clang++ -o bin/main \
 -Wall \
--I/users/labnet4/st6/djp468/.sfml/include \
+-I./include \
 src/astar.cpp \
 src/bfs.cpp \
 src/gui.cpp \
@@ -9,9 +10,15 @@ src/main.cpp \
 src/map.cpp \
 src/pathfinding.cpp \
 src/tile.cpp \
--L/users/labnet4/st6/djp468/.sfml/lib \
+include/imgui.cpp \
+include/imgui_draw.cpp \
+include/imgui-SFML.cpp \
+include/imgui_demo.cpp \
+-L./lib \
 -lsfml-graphics \
 -lsfml-window \
--lsfml-system
+-lsfml-system \
+-lGL
 cd bin
+export LD_LIBRARY_PATH=../lib
 ./main
