c06489ef77694bd71aa4e3586333061899328dfd
infamousrob@gmail.com <Github!123>
2018-12-14 13:33:42 -0330

    beginning jps

--- src/jps.cpp
+++ src/jps.cpp
@@ -49,15 +49,26 @@ void JPS::step()
 	uint idx = curNode->tile->getIndex();
 	uint cost = curNode->getCost();
 	closedList[idx] = cost;
-	// iter neighbours
+
+	//iter neighbours
 	for (auto tile : map.getLegalNeighbours(curNode->tile))
 	{
+		//make node, set parent, calculate hCost
 		auto n = std::make_shared<AStarNode>(tile);
 		n->parent = curNode;
 		n->hCost = calcHScore(n->tile);
-		n->gCost = map.isDiagonal(curNode->tile, n->tile) ? curNode->gCost + 141 : curNode->gCost + 100;
+		bool isDiag = map.isDiagonal(curNode->tile, n->tile);
+		n->gCost = isDiag ? curNode->gCost + 141 : curNode->gCost + 100;
+		//check to see if this node already exists on the open list with a lower cost
 		if (n->gCost < openLookup[n->tile->getIndex()])
 		{
+			//JPS scan this new node
+			if (!isDiag)
+			{
+				auto dx = n->tile->getX(map.getWidth()) - curNode->tile->getX(map.getWidth());
+				auto dy = n->tile->getY(map.getWidth()) - curNode->tile->getY(map.getWidth());
+				cardinalScan(n, dx, dy);
+			}
 			openLookup[n->tile->getIndex()] = n->gCost;
 			openList.push(n);
 		}
@@ -88,6 +99,78 @@ tileArray JPS::getClosedList()
 	return out;
 }
 
+//Jump Point Scan for cardinal directions, adds a consecutive series of nodes to the open list
+void JPS::cardinalScan(std::shared_ptr<AStarNode> node, int dx, int dy)
+{
+	//origin node
+	const int DELTA_COST = 100;
+	//auto curNode = node;
+	auto curTile = node->tile;
+	bool addNodes = false; //jps terminates when parallel nodes to the path have been added
+	auto sector = curTile->getSector();
+	uint cost = 0;
+	while (true)
+	{
+		auto ox = curTile->getX(map.getWidth());
+		auto oy = curTile->getY(map.getWidth());
+		//node we're advancing to with the scan
+		auto nextX = ox + dx;
+		auto nextY = oy + dx;
+		//check termination conditions before calculating jps stuff
+		if (map.isOOB(nextX, nextY) || sector != map.get(nextX, nextY)->getSector()) return;
+		if (curTile->getIndex() == destination)
+		{
+			auto endNode = std::make_shared<AStarNode>(curTile);
+			Pathfinding::makePath(*endNode); //todo update the method to only take refs
+			status = FINISHED;
+			return;
+		}
+		//nodes adjacent to the origin (ie if moving L to R these are the nodes on top and bottom of the origin)
+		auto adjX1 = ox + dy;
+		auto adjY1 = oy + dx;
+		auto adjX2 = ox + -dy;
+		auto adjY2 = oy + -dx;
+		//nodes adjacent to the advanced node
+		auto adjNextX1 = nextX + dy;
+		auto adjNextY1 = nextY + dx;
+		auto adjNextX2 = nextX + -dy;
+		auto adjNextY2 = nextY + -dx;
+		cost += DELTA_COST; //cost of 
+		//check for the potential of parallel nodes
+		if (sector != map.get(adjX1, adjY1)->getSector() && sector == map.get(adjNextX1, adjNextY1)->getSector())
+		{
+			addNodes = true;
+			auto adjNode1 = std::make_shared<AStarNode>(map.get(adjNextX1, adjNextY1));
+			adjNode1->gCost = cost;
+			adjNode1->hCost = calcHScore(adjNode1->tile);
+			openLookup[adjNode1->tile->getIndex()] = adjNode1->gCost;
+			openList.push(adjNode1);
+		}
+		if (sector != map.get(adjX2, adjY2)->getSector() && sector == map.get(adjNextX2, adjNextY2)->getSector())
+		{
+			addNodes = true;
+			auto adjNode2 = std::make_shared<AStarNode>(map.get(adjNextX2, adjNextY2));
+			adjNode2->gCost = cost;
+			adjNode2->hCost = calcHScore(adjNode2->tile);
+			openLookup[adjNode2->tile->getIndex()] = adjNode2->gCost;
+			openList.push(adjNode2);
+		}
+		if (addNodes) //if we found a parallel node we add the next node to the list and return to pop the lowest score
+		{
+			auto nextNode = std::make_shared<AStarNode>(map.get(nextX,nextY));
+			nextNode->gCost = cost; 
+			nextNode->hCost = calcHScore(nextNode->tile);
+			openLookup[nextNode->tile->getIndex()] = nextNode->gCost;
+			openList.push(nextNode);
+			return;
+		}
+		//no new nodes were added and the path is clear: continue 
+		curTile = map.get(nextX, nextY);
+	}
+
+
+}
+
 std::vector<std::shared_ptr<PathNode>> JPS::getOpenNodes()
 {
 	std::vector<std::shared_ptr<PathNode>> out;
@@ -101,5 +184,5 @@ std::vector<std::shared_ptr<PathNode>> JPS::getOpenNodes()
 float JPS::calcHScore(const std::shared_ptr<Tile>& t)
 {
 	//todo pick between heuristics
-	return euclidean(map, t, map.get(destination))*100;
+	return euclidean(map, t, map.get(destination)) * 100;
 }
